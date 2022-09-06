b00c9410e53dcaceffff719cb6f4c767f27e2035
Diego Leon <diegoleondss@hotmail.com>
2022-09-06 10:32:48 -0300

    userListViewModel tested

--- app/src/test/java/com/picpay/desafio/android/ui/userlist/UserListViewModelTest.kt
+++ app/src/test/java/com/picpay/desafio/android/ui/userlist/UserListViewModelTest.kt
@@ -1,9 +1,11 @@
 package com.picpay.desafio.android.ui.userlist
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import com.picpay.desafio.android.data.repository.UserRepository
 import com.picpay.desafio.android.data.wrapper.Result
+import com.picpay.desafio.android.domain.model.user.DomainUser
+import com.picpay.desafio.android.domain.usecase.FetchUsersUseCase
 import com.picpay.desafio.android.util.MainCoroutineRule
+import io.mockk.coEvery
 import io.mockk.mockk
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.delay
@@ -11,11 +13,24 @@ import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.runBlocking
 import org.junit.Rule
 import org.junit.Test
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
 
 internal class UserListViewModelTest {
-    private val repository: UserRepository = mockk()
+    private val useCase: FetchUsersUseCase = mockk()
     private lateinit var viewModel: UserListViewModel
+
+    private val user = DomainUser(
+        id = 1L,
+        username = "username",
+        name = "name",
+        img = "img.com"
+    )
+    private val users = listOf(
+        user,
+        user.copy(id = 2L),
+        user.copy(id = 3L)
+    )
 
     @get:Rule
     val instantExecutorRule = InstantTaskExecutorRule()
@@ -24,36 +39,47 @@ internal class UserListViewModelTest {
     @get:Rule
     val coroutineScope = MainCoroutineRule()
 
-    @BeforeEach
-    fun initViewModel() {
-        viewModel = UserListViewModel(repository)
-    }
+    @Test
+    fun `Should set uiState as Success when useCase fetch is successful`() =
+        runBlocking {
+            val flow = flow<Result<List<DomainUser>>> {
+                emit(Result.Success(users))
+            }
+            coEvery { useCase.fetchUsers() } returns flow
+            initViewModel()
+
+            assertTrue(viewModel.uiState.value is UiState.Success)
+            assertEquals(users, (viewModel.uiState.value as UiState.Success).users)
+        }
 
     @Test
-    fun `Should set uiState as Success when repository fetch is successful and not return an empty list`() =
+    fun `Should set uiState as Fail when useCase fetch fails and uiState not have a previous success value`() =
+        runBlocking {
+            val flow = flow<Result<List<DomainUser>>> {
+                emit(Result.Fail(mockk()))
+            }
+            coEvery { useCase.fetchUsers() } returns flow
+            initViewModel()
+
+            assertTrue(viewModel.uiState.value is UiState.Error)
+        }
+
+    @Test
+    fun `Should not set uiState as Fail when useCase fetch fails and uiState have a previous success value`() =
         runBlocking {
             val flow = flow {
-                emit(2)
-                delay(10)
-                emit(1)
+                emit(Result.Success(users))
+                emit(Result.Fail(mockk()))
             }
+            coEvery { useCase.fetchUsers() } returns flow
+            initViewModel()
+
+            assertFalse(viewModel.uiState.value is UiState.Error)
+            assertTrue(viewModel.uiState.value is UiState.Success)
+            assertEquals(users, (viewModel.uiState.value as UiState.Success).users)
         }
 
-    @Test
-    fun `Should not set uiState as Success when repository fetch is successful and not return an empty list`() =
-        runBlocking {
-
-        }
-
-    @Test
-    fun `Should set uiState as Fail when repository fetch fails and uiState not have a previous success value`() =
-        runBlocking {
-
-        }
-
-    @Test
-    fun `Should not set uiState as Fail when repository fetch fails and uiState have a previous success value`() =
-        runBlocking {
-
-        }
+    private fun initViewModel() {
+        viewModel = UserListViewModel(useCase)
+    }
 }
